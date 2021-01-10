b53cb6887dc5baefdc656ab07b2b5918609e48dd
kosmolet <svet1199@gmail.com>
2021-01-10 02:30:33 +0100

    add mongoose middleware and login/register

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,17 +1,28 @@
+const crypto = require("crypto");
 const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-exports.register = async (req, res, next) => {
+exports.register = async (req, res) => {
   const { username, email, password } = req.body;
   try {
-    const user = await User.create({
+    let user = await User.findOne({ email });
+    if (user) {
+      return res
+        .status(400)
+        .json({ error: "User with this email already exists" });
+    }
+
+    user = new User({
       username,
       email,
       password,
     });
 
+    const newUser = await user.save();
     res.status(201).json({
       success: true,
-      user,
+      user: newUser,
     });
   } catch (err) {
     res.status(500).json({
@@ -21,14 +32,51 @@ exports.register = async (req, res, next) => {
   }
 };
 
-exports.login = async (req, res, next) => {
-  res.send("login");
+exports.login = async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    res
+      .status(400)
+      .json({ success: false, error: "Please provide email and password" });
+  }
+  try {
+    const user = await User.findOne({ email }).select("+password");
+
+    if (!user) {
+      return res
+        .status(400)
+        .json({ success: false, error: "Email! or password are invalid" });
+    }
+
+    //const isPasswordMatch = await bcrypt.compare(password, user.password);
+    const isPasswordMatch = await user.matchPassword(password);
+    if (!isPasswordMatch) {
+      return res
+        .status(400)
+        .json({ success: false, erorr: "Email or password! are invalid" });
+    }
+
+    // const payload = {
+    //   user: {
+    //     userId: user._id,
+    //   },
+    // };
+
+    // const token = jwt.sign(payload, JWT_SECRET, {
+    //   expiresIn: "1hr",
+    // });
+    const token = user.getSignedJwtToken();
+
+    res.status(200).json({ success: true, token });
+  } catch (err) {
+    return res.status(500).json({ success: false, message: err.message });
+  }
 };
 
-exports.forgotPassword = async (req, res, next) => {
+exports.forgotPassword = async (req, res) => {
   res.send("forgot");
 };
 
-exports.resetPassword = async (req, res, next) => {
+exports.resetPassword = async (req, res) => {
   res.send("reset");
 };
--- models/User.js
+++ models/User.js
@@ -1,13 +1,21 @@
+const crypto = require("crypto");
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const userSchema = new Schema(
   {
-    username: { type: String, required: [true, "Please provide username"] },
+    username: {
+      type: String,
+      trim: true,
+      required: [true, "Please provide username"],
+    },
     email: {
       type: String,
       unique: true,
       required: [true, "Please provide email"],
+      trim: true,
     },
     password: {
       type: String,
@@ -21,5 +29,21 @@ const userSchema = new Schema(
 
   { timestamps: true }
 );
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 12);
+  next();
+});
+
+userSchema.methods.matchPassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.getSignedJwtToken = function () {
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRE,
+  });
+};
 
 module.exports = mongoose.model("User", userSchema);
